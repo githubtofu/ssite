560990c1f7c4fe63aef2a8280aa86d8b92b01734
obviouswinter30 <slowdrop@gmail.com>
2025-01-17 10:56:16 +0900

    tnode to hnode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from textnode import *
 
+def text_node_to_html_node(text_node):
+    match text_node:
+        case text_node.TEXT:
+            return LeafNode(text_node.value)
+        case text_node.BOLD:
+            return LeafNode("b", text_node.value)
+        case text_node.ITALIC:
+            return LeafNode("i", text_node.value)
+        case text_node.CODE:
+            return LeafNode("i", text_node.value)
+        case text_node.LINK:
+            return LeafNode("a", text_node.value, {"href":text_node.url})
+        case text_node.IMAGE:
+            return LeafNode("img", {"src":text_node.url, "alt":text_node.value})
+            
 def main():
     my_node = TextNode("Test node", TextType.BOLD, "https:/nonese")
     print(my_node)
 
-main()
+
+if __name__ == "__main__":
+    main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,22 @@
 import unittest
 
 from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL:
+            return LeafNode(text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("i", text_node.text)
+        case TextType.LINKS:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case TextType.IMAGES:
+            return LeafNode("img", {"src":text_node.url, "alt":text_node.text})
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +39,14 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.ITALIC)
         node2 = TextNode("This is a text node", TextType.ITALIC, None)
         self.assertEqual(node, node2)
+
+    def test_t2h(self):
+        node_italic = text_node_to_html_node(TextNode("This is a text node", TextType.ITALIC))
+        node_link = text_node_to_html_node(TextNode("This is a text node", TextType.LINKS, "http link link"))
+        node_image = text_node_to_html_node(TextNode("This is a text node", TextType.IMAGES, "http link link"))
+        print(f"ITALIC:{node_italic}:")
+        print(f"LINK:{node_link}:")
+        print(f"IMAGE:{node_image}:")
 
 if __name__ == "__main__":
     unittest.main()
