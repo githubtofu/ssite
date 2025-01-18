4553cd5b195b4dd0b8f2d76e85b9bfe25515aa8d
tofuman <slowdrop@gmail.com>
2025-01-18 11:47:02 +0900

    block 2 type ing

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,4 @@
+import re
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     for a_block in blocks:
@@ -8,3 +9,20 @@ def markdown_to_blocks(markdown):
         else:
             break
     return blocks
+
+def block_to_block_type(block_text):
+    print("tbt_q IN function print text")
+    print(block_text)
+    print("tbt_q" + "=" * 30)
+    r = re.compile(r"#{1,6} .+")
+    if r.match(block_text) != None:
+        return "heading"
+    r = re.compile(r"```.*?```$")
+    if r.match(block_text) != None:
+        return "code"
+    r = re.compile(r"^[^>]", re.MULTILINE)
+    if r.search(block_text) == None:
+        return "quote"
+    r = re.compile(r"")
+    
+    return "paragraph"
--- src/test_block_md.py
+++ src/test_block_md.py
@@ -2,17 +2,44 @@ import unittest
 from block_markdown import *
 
 class TestTextNode(unittest.TestCase):
-    def test_block_simple(self):
-        test_str = """# This is a heading
+
+    test_str = """# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
 * This is the first list item in a list block
 * This is a list item
 * This is another list item"""
-        print(test_str)
-        print(markdown_to_blocks(test_str))
 
+    def test_block_simple(self):
+        print(self.test_str)
+        print(markdown_to_blocks(self.test_str))
 
+    def test_block_type(self):
+        t = block_to_block_type("parapara")
+        print("tbt" + "*" * 24)
+        print("tbt Testing block type check : heading")
+        for i in range(9):
+            print("tbt " + block_to_block_type("#" * i + " teet"))
+        print("tbt" + "*" * 24)
+        
+    def test_block_type_code(self):
+        test_head = "tbt_c"
+        print(test_head + " " + "*" * 24)
+        print(test_head + " Testing block type check : heading")
+        print(test_head + " " + block_to_block_type("``` should be code ```"))
+        print(test_head + " " + block_to_block_type("``` should NOT be code ```djkf"))
+        print(test_head + " " + "*" * 24)
 
-
+    def test_block_type_quote(self):
+        test_head = "tbt_q"
+        tstr = """>>>quote
+>>>quotoeoe """
+        print(test_head + " " + "*" * 24)
+        print(test_head + " Testing block type check : quote")
+        print(test_head + " " + block_to_block_type(tstr))
+        tstr = """>>>quote
+quotoeoe 
+        kdjkdk"""
+        print(test_head + " " + block_to_block_type(tstr))
+        print(test_head + " " + "*" * 24)
