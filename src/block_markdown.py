c000973aabc75fce232b6c8282f104375c27d34d
obviouswinter30 <slowdrop@gmail.com>
2025-02-10 14:05:01 +0900

    recent

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     for a_block in blocks:
@@ -8,3 +10,6 @@ def markdown_to_blocks(markdown):
         else:
             break
     return blocks
+
+def block_to_block_type(block_str):
+    r = re.compile(r"")
